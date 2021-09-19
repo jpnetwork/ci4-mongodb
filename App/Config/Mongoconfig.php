192eaa36ec4d05700807adf5d66fd1d127ad831c
JPNET <43113652+jpnetwork@users.noreply.github.com>
2021-09-20 05:43:17 +0700

    Update Mongoconfig.php

--- App/Config/Mongoconfig.php
+++ App/Config/Mongoconfig.php
@@ -5,7 +5,7 @@ use CodeIgniter\Config\BaseConfig;
 class MongoConfig extends BaseConfig
 {
     
-    public $db = "sujane";//ชื่อฐานข้อมูล
+    public $db = "test";//ชื่อฐานข้อมูล
     public $hostname = '127.0.0.1'; 
     public $password = "";
     public $prefix = "";
