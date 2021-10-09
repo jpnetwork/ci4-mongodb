2539a591fad7ff817d8e9b9fc29f2eba5ef47602
JPNET <43113652+jpnetwork@users.noreply.github.com>
2021-10-09 11:01:34 +0700

    Update missing parameter

--- App/Config/Mongoconfig.php
+++ App/Config/Mongoconfig.php
@@ -5,8 +5,9 @@ use CodeIgniter\Config\BaseConfig;
 class MongoConfig extends BaseConfig
 {
     
-    public $db = "test";//ชื่อฐานข้อมูล
+    public $db = "DB";//ชื่อฐานข้อมูล
     public $hostname = '127.0.0.1'; 
+    public $userName = "test_user";
     public $password = "";
     public $prefix = "";
     public $port = 27017; 
