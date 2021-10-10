a40662f2fd7f87def26d9425d940b44250f124c5
JPNET <43113652+jpnetwork@users.noreply.github.com>
2021-10-11 06:17:09 +0700

    Add missing $userName

--- App/Config/Mongoconfig.php
+++ App/Config/Mongoconfig.php
@@ -7,6 +7,7 @@ class MongoConfig extends BaseConfig
     
     public $db = "test";//ชื่อฐานข้อมูล
     public $hostname = '127.0.0.1'; 
+    public $userName = "";
     public $password = "";
     public $prefix = "";
     public $port = 27017; 
